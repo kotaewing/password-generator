f89bf4a4ce34634f8e28d875837718627a1d3c70
kotaewing <kota.ewing@gmail.com>
2021-04-01 13:42:55 -0600

    defined new functionsfor character type

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generate password function defined
+function generatePassword() {
+  var length = prompt('How many characters would you like your password to be?  You may choose any length between 8 and 128');
+  var charType = characterType();
+}
+
+
+function characterType() {
+  var lower = confirm('Would you like lowercase characters? Choose OK for yes, or Cancel for no.')
+  var upper = confirm('Would you like uppercase characters? Choose OK for yes, or Cancel for no.')
+  var numeric = confirm('Would you like numeric characters? Choose OK for yes, or Cancel for no.')
+  var special = confirm('Would you like special characters? Choose OK for yes, or Cancel for no.')
+}
